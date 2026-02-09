555617d5c41f93ce17fe471fe23089d414255ae7
Vai Srivastava <vaisriv@icloud.com>
2026-02-09 15:45:25 -0500

    feat(example): make example fully compilable

---
+++ example/src/p03.py
@@ -0,0 +1,17 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+
+
+def main():
+    print("Hello World!")
+
+    #######
+    # p03 #
+    #######
+
+    x = np.linspace(0, 10, 100)
+    y = np.sin(x)
+
+if __name__ == "__main__":
+    main()
