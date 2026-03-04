61aefe371831d6d5869f5da309bb70152b947400
Vai Srivastava <vaisriv@icloud.com>
2026-03-04 17:42:00 -0500

    feat(nix): move from alejandra to nixfmt for nix formatter

--- nix/formatter.nix
+++ nix/formatter.nix
@@ -6,27 +6,31 @@
 inputs.treefmt-nix.lib.mkWrapper pkgs {
     projectRootFile = "flake.nix";
 
-    # nix fmt
+    # nix
     programs = {
         deadnix.enable = true;
-        alejandra.enable = true;
-    };
-    settings.formatter = {
-        alejandra = {
-            options = [
-                "--experimental-config"
-                "./nix/fmt/alejandra.toml"
-            ];
+        nixfmt = {
+            enable = true;
+            indent = 4;
         };
     };
 
-    # python
-    programs = {
-        black.enable = true;
+    # md
+    programs.prettier = {
+        enable = true;
+        settings = {
+            tabWidth = 4;
+        };
     };
 
     # latex
     programs = {
         latexindent.enable = true;
+    };
+
+    # python
+    programs = {
+        ruff-check.enable = true;
+        ruff-format.enable = true;
     };
 }
