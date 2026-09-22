77e571764eb39045792bb6aa583c103f766ba9af
Vai Srivastava <vaisriv@icloud.com>
2026-09-22 15:34:28 -0400

    feat(nix): add flake config

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+    description = "hw-template flake";
+
+    inputs = {
+        # submodules
+        self.submodules = true;
+
+        # nixpkgs
+        nixpkgs.url = "github:nixos/nixpkgs/release-26.05";
+        nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+
+        # flake tools (thanks numtide)
+        blueprint = {
+            url = "github:numtide/blueprint";
+            inputs.nixpkgs.follows = "nixpkgs";
+        };
+        devshell = {
+            url = "github:numtide/devshell";
+            inputs.nixpkgs.follows = "nixpkgs";
+        };
+        treefmt-nix = {
+            url = "github:numtide/treefmt-nix";
+            inputs.nixpkgs.follows = "nixpkgs";
+        };
+    };
+
+    outputs =
+        inputs:
+        inputs.blueprint {
+            inherit inputs;
+            prefix = "./nix/";
+        };
+}
